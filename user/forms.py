62b5b58f0cb3898cec26abe449c9b88b92111082
mekroket <67954467+mekroket@users.noreply.github.com>
2021-02-22 14:20:05 +0300

    güncelleme

--- user/forms.py
+++ user/forms.py
@@ -3,31 +3,41 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from user import urls
+from django.contrib.auth.models import User
 
 
-class  RegisterForm(forms.Form):
-    username = forms.CharField(max_length=50,label="Kullanıcı Adı")
-    
-    password = forms.CharField(max_length=20,label="Şifre",widget=forms.PasswordInput)
-    confirm = forms.CharField(max_length=20,label="Şifrenizi Doğrulayın",widget=forms.PasswordInput)
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={"class": "input-text","placeholder":"Kullanıcı Adı"}
+    ),required=True,max_length=50)
 
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={"class":"input-text","placeholder":"Email"}
+    ),required=True,max_length=50)
 
-    #Kullanıcıyı çekiyoruz
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"class":"input-text","placeholder":"Şifre"}
+    ),required=True,max_length=50)
+
+    class Meta():
+        model = User
+        fields = ["username","email","password"]
+
     def clean(self):
         username = self.cleaned_data.get("username")
         password = self.cleaned_data.get("password")
-        
-        confirm = self.cleaned_data.get("confirm")
+        email = self.cleaned_data.get("email")
 
-        #Şartlı durumları yazıyoruz
-        if password and confirm and password !=confirm :
-            raise forms.ValidationError("Parolalar Eşleşmiyor")
-        
-        #Girilen değerleri sözlüğe ekliyoruz
         values = {
-            "username":username,
-            "password":password,
+            "username" : username,
+            "password" : password,
+            "email":email,
             
         }
         return values
 
+class LoginForm(forms.Form):
+    username = forms.CharField(label = "Kullanıcı Adı")
+    password = forms.CharField(label = "Parola",widget = forms.PasswordInput)
+    
+    
--- user/views.py
+++ user/views.py
@@ -1,41 +1,79 @@
 from .forms import RegisterForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate,logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from .forms import LoginForm
+
+
 # Create your views here.
 
-#kayıt olma
+#ÖZEL KAYIT OLUŞTURMA BÖYLE YAPILIYOR
+
 def register(request):
+
     form = RegisterForm(request.POST or None)
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
-
-        newUser = User(username=username)
+        newUser = User(username = username)
         newUser.set_password(password)
+
         newUser.save()
         login(request,newUser)
-        messages.success(request,"Başarıyla Kayıt Olundu")
+        messages.info(request,"Başarıyla Kayıt Oldunuz...")
 
         return redirect("index")
-
     context = {
-        "form": form
+            "form" : form
         }
     return render(request,"register.html",context)
 
-
-
-
-
-
-
-
-
+    
+    
 def loginUser(request):
-    return render(request,"login.html")
+    form = LoginForm(request.POST or None)
+
+    context = {
+        "form":form
+    }
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        
+
+        user = authenticate(username = username,password = password )
+
+        if user is None:
+            messages.info(request,"Kullanıcı Adı veya Parola Hatalı")
+            return render(request,"login.html",context)
+
+        messages.success(request,"Başarıyla Giriş Yaptınız")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def logoutUser(request):
-    return render(request,"logout.html")
+    logout(request)
+    messages.success(request,"Başarıyla Çıkış Yaptınız")
+    return redirect("index")
